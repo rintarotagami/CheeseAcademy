7a7474ef3aea2cbed7461ba3070ac580d88c2142
Rin <rintarotagami@gmail.com>
2024-05-07 10:23:27 +0900

    サーバーに上げたとき、パスが参照できていない問題の修正

--- js/sounds.js
+++ js/sounds.js
@@ -1,21 +1,27 @@
 //音楽ファイルのインスタンス作成------------------------------------------
 // オーディオ要素を作成し、DOMに追加する関数
 function createAndAppendAudio(globalKey, src) {
-    window[globalKey] = new Audio(src);
+    let basePath = '';
+    if (window.location.hostname === 'rin-study.sakura.ne.jp') {
+        basePath = 'https://rin-study.sakura.ne.jp/cheese-academy/sound/';
+    } else {
+        basePath = '../sound/';
+    }
+    window[globalKey] = new Audio(basePath + src);
     document.body.appendChild(window[globalKey]);
 }
 
 // オーディオ要素の作成と追加
-createAndAppendAudio('audioPlayer', '../sound/EscapeTheChase.mp3');
-createAndAppendAudio('wallAudio', '../sound/zubashu.mp3');
-createAndAppendAudio('toBeContinued', 'sound/toBeContinued.mp3');
-createAndAppendAudio('playAudio', '../sound/play.mp3');
-createAndAppendAudio('cursorSound', '../sound/cursor_1.mp3');
-createAndAppendAudio('araraSound', '../sound/arara.mp3');
-createAndAppendAudio('poisonSound', '../sound/poison.mp3');
-createAndAppendAudio('blokenSound', '../sound/bloken.mp3');
-createAndAppendAudio('victorySound', '../sound/victory.mp3');
-createAndAppendAudio('rainbowSound', '../sound/rainbow.mp3');
+createAndAppendAudio('audioPlayer', 'EscapeTheChase.mp3');
+createAndAppendAudio('wallAudio', 'zubashu.mp3');
+createAndAppendAudio('toBeContinued', 'toBeContinued.mp3');
+createAndAppendAudio('playAudio', 'play.mp3');
+createAndAppendAudio('cursorSound', 'cursor_1.mp3');
+createAndAppendAudio('araraSound', 'arara.mp3');
+createAndAppendAudio('poisonSound', 'poison.mp3');
+createAndAppendAudio('blokenSound', 'bloken.mp3');
+createAndAppendAudio('victorySound', 'victory.mp3');
+createAndAppendAudio('rainbowSound', 'rainbow.mp3');
 
 
 // rainbowSoundとblokenSoundの音量を80%に設定
